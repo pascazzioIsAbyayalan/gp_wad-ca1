22f7044e48c61199d7a2a437d7a178bf46930067
pascazzio <pascaciogre@gmail.com>
2026-03-08 23:47:36 +0000

    Add Routes

--- routes.js
+++ routes.js
@@ -8,7 +8,12 @@ const router = express.Router();
 // add your own routes below
 
 import start from './controllers/start.js';
+import dashboard from './controllers/dashboard.js';
+import about from './controllers/about.js';
+
 router.get('/', start.createView);
+router.get('/dashboard', dashboard.createView);
+router.get('/about', about.createView);
 
 
 export default router;
